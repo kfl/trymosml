30d7b327491eaed86d60a96ced4e5814eb5853eb
Ken Friis Larsen <ken@friislarsen.net>
2014-08-14 12:11:25 +0200

    Clean up code for `Save` and started now `New` menu items

--- web/trymosml.js
+++ web/trymosml.js
@@ -142,6 +142,10 @@ var trymosml = function(){
         });
     };
 
+    function newFile() {
+
+    };
+
     function loadLocalFileInEditor(evt) {
         var file = evt.target.files[0]; 
 
@@ -164,23 +168,14 @@ var trymosml = function(){
         var textFileAsBlob = new Blob([content], {type:'text/plain'});
         var fileNameToSaveAs = $(".buffer-name").text();
 
-        var downloadLink = document.createElement("a");
+        var downloadLink = document.createElement('a');
         downloadLink.download = fileNameToSaveAs;
-        downloadLink.innerHTML = "Download File";
-        if (window.webkitURL != null) {
-            // Chrome allows the link to be clicked
-            // without actually adding it to the DOM.
-            downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-        } else {
-            // Firefox requires the link to be added to the DOM
-            // before it can be clicked.
-            downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-            downloadLink.onclick = function (event) {
-	            document.body.removeChild(event.target);
-            };
-            downloadLink.style.display = "none";
-            document.body.appendChild(downloadLink);
-        }
+        downloadLink.href = URL.createObjectURL(textFileAsBlob);
+        downloadLink.style.display = 'none';
+        downloadLink.onclick = function (event) {
+            document.body.removeChild(event.target);
+        };
+        document.body.appendChild(downloadLink);
         downloadLink.click();
     };
 
